2d2e88f0f57877e40cb66ec8c76bc91e9a33b56e
magimenon <magicmenon>
2015-02-23 15:54:19 -0800

    Distraction - Mobile App

---
+++ platforms/ios/www/js/rangeWeek.js
@@ -0,0 +1,7 @@
+function rangeWeek (dateStr) {
+    if (!dateStr) dateStr = new Date().getTime();
+    var dt = new Date(dateStr);
+    dt = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate());
+    dt = new Date(dt.getTime() - (dt.getDay() > 0 ? (dt.getDay() - 1) * 1000 * 60 * 60 * 24 : 6 * 1000 * 60 * 60 * 24));
+    return { start: dt, end: new Date(dt.getTime() + 1000 * 60 * 60 * 24 * 7 - 1) };
+}
